aeb8a3b16e32305ba28b85444073aa2dbacb6474
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-09-16 22:30:05 +0300

    configured cors

--- app.js
+++ app.js
@@ -2,18 +2,20 @@ const cookieParser = require('cookie-parser');
 const express = require('express');
 const httpErrors = require('http-errors');
 const logger = require('morgan');
-const path = require('path');
-
+// const path = require('path');
+const cors =require ('cors')
+require('dotenv').config();
 const indexRouter = require('./routes/index');
 
 const app = express();
-
+app.use(cors());
+app.use(express.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
